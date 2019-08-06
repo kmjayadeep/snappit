d396e816a57ffde6d1dde751dd1a735b59324754
Aswath K <aswath.sana@gmail.com>
2019-08-06 20:43:45 +0530

    Enables CORS in the development mode

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const router = express.Router();
 const snipApi = require('./snip');
 
+if (express().get('env') == 'development') {
+  router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
+
 router.use('/snip', snipApi);
 
 module.exports = router;
